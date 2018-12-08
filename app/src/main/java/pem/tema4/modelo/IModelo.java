446331415e79567bfc43788967f1a6929e6bf083
eche23 <eche2312@gmail.com>
2018-12-08 17:50:17 +0000

    Initial commit

---
+++ app/src/main/java/pem/tema4/modelo/IModelo.java
@@ -0,0 +1,18 @@
+package pem.tema4.modelo;
+
+public interface IModelo {
+	
+	// TODO Añadir el método obtenerDatos() que recupera los datos a mostrar en la lista maestro.
+    void obtenerDatos();
+
+	// TODO Añadir el método obtenerDetalles() que recupera los datos de una receta dada su posición
+	// en la lista maestro.
+    void obtenerDetalle(int posicion);
+
+	// TODO Añadir el método agregarReceta(Object[] datos) que almacena una nueva receta en la lista
+	// de recetas.
+    void agregarReceta(Object[] datos);
+
+}
+
+
---
+++ app/src/main/java/pem/tema4/vista/Adaptador.java
@@ -0,0 +1,59 @@
+package pem.tema4.vista;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.FilaViewHolder> {
+
+    private String[] items;
+    private SeleccionListener oyente;
+
+    public Adaptador(Object[] datos, SeleccionListener oyente) {
+        items = (String[])datos;
+        this.oyente = oyente;
+    }
+
+    public interface SeleccionListener {
+        public void onClick(FilaViewHolder fvh, int posicion);
+    }
+
+    @Override
+    public FilaViewHolder onCreateViewHolder(ViewGroup padre, int viewType) {
+        View view = LayoutInflater.from(padre.getContext())
+                .inflate(R.layout.layout_item_maestro, padre, false);
+        return new FilaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FilaViewHolder fvh, int posicion) {
+        fvh.item.setText(items[posicion]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.length;
+    }
+
+    public class FilaViewHolder extends RecyclerView.ViewHolder  {
+
+        private TextView item;
+
+        public FilaViewHolder(View view) {
+            super(view);
+            view.setClickable(true);
+            item = (TextView) view.findViewById(R.id.item_maestro);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    oyente.onClick(FilaViewHolder.this, getAdapterPosition());
+                }
+            });
+        }
+    }
+
+}
